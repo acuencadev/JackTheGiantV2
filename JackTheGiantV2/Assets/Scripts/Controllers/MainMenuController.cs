4657335cfca8c7c7239f96240a3cebfe25fe4a78
Amador Cuenca <sphi02ac@gmail.com>
2019-07-03 17:50:41 -0500

    Create the MainMenuController
    Create the MainMenuController and associate click event on the buttons.

---
+++ JackTheGiantV2/Assets/Scripts/Controllers/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Mantelabs.JackTheGiant.Controllers
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            Debug.Log("Starting the game...");
+        }
+
+
+        public void Highscore()
+        {
+            Debug.Log("Launching the highscore screen...");
+        }
+
+
+        public void Options()
+        {
+            Debug.Log("Launching the options screen...");
+        }
+
+
+        public void QuitGame()
+        {
+            Debug.Log("Quitting the game...");
+        }
+    }
+}
