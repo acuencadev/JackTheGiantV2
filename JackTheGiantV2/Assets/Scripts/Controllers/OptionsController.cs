0cb06c09d718d409018c01aeb0dc7fec16c676a5
Amador Cuenca <sphi02ac@gmail.com>
2019-07-04 07:58:53 -0500

    Select the difficulty in the options scene
    Toggle the difficulty signs and select the right difficulty.

--- JackTheGiantV2/Assets/Scripts/Controllers/OptionsController.cs
+++ JackTheGiantV2/Assets/Scripts/Controllers/OptionsController.cs
@@ -4,21 +4,38 @@ namespace Mantelabs.JackTheGiant.Controllers
 {
     public class OptionsController : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject _easySign, _mediumSign, _hardSign;
+
+
+        private void Start()
+        {
+            //TODO: Pull the current difficulty from the PlayerPrefs.
+            SetMediumDifficulty();
+        }
+
+
         public void SetEasyDifficulty()
         {
-            Debug.Log("Easy difficulty set.");
+            _easySign.SetActive(true);
+            _mediumSign.SetActive(false);
+            _hardSign.SetActive(false);
         }
 
 
         public void SetMediumDifficulty()
         {
-            Debug.Log("Medium difficulty set.");
+            _easySign.SetActive(false);
+            _mediumSign.SetActive(true);
+            _hardSign.SetActive(false);
         }
 
 
         public void SetHardDifficulty()
         {
-            Debug.Log("Hard difficulty set.");
+            _easySign.SetActive(false);
+            _mediumSign.SetActive(false);
+            _hardSign.SetActive(true);
         }
 
 
