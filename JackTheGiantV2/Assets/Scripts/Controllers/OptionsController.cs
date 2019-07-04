aa31e0abc5447d119d5c3c55f3681dcc5ce79b62
Amador Cuenca <sphi02ac@gmail.com>
2019-07-04 07:35:15 -0500

    Attach the Options controller script to the Options Game Object

---
+++ JackTheGiantV2/Assets/Scripts/Controllers/OptionsController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Mantelabs.JackTheGiant.Controllers
+{
+    public class OptionsController : MonoBehaviour
+    {
+        public void SetEasyDifficulty()
+        {
+            Debug.Log("Easy difficulty set.");
+        }
+
+
+        public void SetMediumDifficulty()
+        {
+            Debug.Log("Medium difficulty set.");
+        }
+
+
+        public void SetHardDifficulty()
+        {
+            Debug.Log("Hard difficulty set.");
+        }
+
+
+        public void GoBackToMainMenu()
+        {
+            Debug.Log("Going back to main menu.");
+        }
+    }
+}
