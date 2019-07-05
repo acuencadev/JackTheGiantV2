dec4a0181f021f138779b255fda4c0bf089a1c71
Amador Cuenca <sphi02ac@gmail.com>
2019-07-05 07:33:47 -0500

    Create the pause panel

---
+++ JackTheGiantV2/Assets/Scripts/Controllers/GameplayController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Mantelabs.JackTheGiant.Controllers
+{
+    public class GameplayController : MonoBehaviour
+    {
+        public void PauseGame()
+        {
+            Debug.Log("Pause the game...");
+        }
+
+
+        public void ResumeGame()
+        {
+            Debug.Log("Resume the game...");
+        }
+
+
+        public void QuitGame()
+        {
+            Debug.Log("Go to Main menu...");
+        }
+    }
+}
