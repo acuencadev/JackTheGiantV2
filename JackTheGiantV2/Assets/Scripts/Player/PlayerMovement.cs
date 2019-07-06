78a54a8c6cbf55b69accf4ab49a09e8124de9189
Amador Cuenca <sphi02ac@gmail.com>
2019-07-06 13:18:26 -0500

    Move the player

--- JackTheGiantV2/Assets/Scripts/Player/PlayerMovement.cs
+++ JackTheGiantV2/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,8 +2,40 @@
 
 namespace Mantelabs.JackTheGiant.Player
 {
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(BoxCollider2D))]
     public class PlayerMovement : MonoBehaviour
     {
-        
+        [SerializeField]
+        private float _speed;
+
+        private Rigidbody2D _rb2d;
+
+
+        private void Awake()
+        {
+            _rb2d = GetComponent<Rigidbody2D>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            float h = Input.GetAxis("Horizontal");
+
+            if (h > 0f)
+            {
+                MovePlayer(Vector2.right);
+            }
+            else if (h < 0f)
+            {
+                MovePlayer(Vector2.left);
+            }
+        }
+
+
+        private void MovePlayer(Vector2 direction)
+        {
+            _rb2d.AddForce(direction * _speed * Time.deltaTime);
+        }
     }
 }
