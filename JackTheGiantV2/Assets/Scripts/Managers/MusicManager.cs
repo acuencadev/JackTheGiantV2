52ac2a0e7fae40ab40eb03fc7d60abcec926a7b4
Amador Cuenca <sphi02ac@gmail.com>
2019-07-06 13:45:22 -0500

    Create the MusicManager

---
+++ JackTheGiantV2/Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Mantelabs.JackTheGiant.Managers
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class MusicManager : MonoBehaviour
+    {
+        public static MusicManager instance;
+
+        [SerializeField]
+        private AudioSource _backgroundMusicSource;
+
+
+        private void Awake()
+        {
+            MakeSingleton();
+        }
+
+
+        private void Start()
+        {
+            //TODO: Remove this, play music ONLY when the setting is ON.
+            PlayBackgroundMusic();
+        }
+
+
+        private void MakeSingleton()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+
+        public void PlayBackgroundMusic()
+        {
+            _backgroundMusicSource.Play();
+        }
+
+
+        public void StopBackgroundMusic()
+        {
+            _backgroundMusicSource.Stop();
+        }
+    }
+}
