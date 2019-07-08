2fcb9d45c6fbc340761c7c4622dc521ffaea3b1d
Amador Cuenca <sphi02ac@gmail.com>
2019-07-08 17:23:26 -0500

    Collect the backgrounds
    Deactivate the background gameObject when hit by the BGCollector (It will be enabled again by the BGSpawner)

--- JackTheGiantV2/Assets/Scripts/Background/BGCollector.cs
+++ JackTheGiantV2/Assets/Scripts/Background/BGCollector.cs
@@ -4,6 +4,12 @@ namespace Mantelabs.JackTheGiant.Background
 {
     public class BGCollector : MonoBehaviour
     {
-        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Background"))
+            {
+                other.gameObject.SetActive(false);
+            }
+        }
     }
 }
