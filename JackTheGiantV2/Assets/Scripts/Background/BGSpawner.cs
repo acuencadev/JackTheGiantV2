223cdb995d4d7fc48f85f31ce3c1fe465d5248fe
Amador Cuenca <sphi02ac@gmail.com>
2019-07-08 17:32:23 -0500

    Initialize the backgrounds last Y position

--- JackTheGiantV2/Assets/Scripts/Background/BGSpawner.cs
+++ JackTheGiantV2/Assets/Scripts/Background/BGSpawner.cs
@@ -7,10 +7,29 @@ namespace Mantelabs.JackTheGiant.Background
     {
         private GameObject[] _backgrounds;
 
+        private float _lastY;
 
         private void Awake()
         {
             _backgrounds = GameObject.FindGameObjectsWithTag(Tags.GameObjects.Background);    
+        }
+
+
+        private void Start()
+        {
+            InitializeLastY();
+        }
+
+
+        private void InitializeLastY()
+        {
+            for (int i = 0; i < _backgrounds.Length; i++)
+            {
+                if (_lastY > _backgrounds[i].transform.position.y)
+                {
+                    _lastY = _backgrounds[i].transform.position.y;
+                }
+            }
         }
     }
 }
