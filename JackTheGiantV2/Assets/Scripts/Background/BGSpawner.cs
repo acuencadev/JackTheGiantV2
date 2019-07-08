4e06d4d5b0cb89f4f13c0f8549e60c54c96f9371
Amador Cuenca <sphi02ac@gmail.com>
2019-07-08 17:25:09 -0500

    Initialize the BGSpawner

--- JackTheGiantV2/Assets/Scripts/Background/BGSpawner.cs
+++ JackTheGiantV2/Assets/Scripts/Background/BGSpawner.cs
@@ -4,6 +4,12 @@ namespace Mantelabs.JackTheGiant.Background
 {
     public class BGSpawner : MonoBehaviour
     {
-        
+        private GameObject[] _backgrounds;
+
+
+        private void Awake()
+        {
+            _backgrounds = GameObject.FindGameObjectsWithTag("Background");    
+        }
     }
 }
