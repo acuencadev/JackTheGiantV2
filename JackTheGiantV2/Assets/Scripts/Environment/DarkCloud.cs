30ddfb8ab787644a9dbe658d1223f02f56b1786d
Amador Cuenca <sphi02ac@gmail.com>
2019-07-11 07:47:00 -0500

    Move the DarkCloud

--- JackTheGiantV2/Assets/Scripts/Environment/DarkCloud.cs
+++ JackTheGiantV2/Assets/Scripts/Environment/DarkCloud.cs
@@ -4,6 +4,35 @@ namespace Mantelabs.JackTheGiant.Environment
 {
     public class DarkCloud : MonoBehaviour
     {
-        
+        [SerializeField]
+        private float _speed = 2f;
+
+        [SerializeField]
+        private float _minX, _maxX;
+
+        private bool _movingLeft;
+
+
+        private void Update()
+        {
+            if (_movingLeft)
+            {
+                transform.Translate(Vector3.left * _speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * _speed * Time.deltaTime);
+            }
+
+            if (transform.position.x <= _minX)
+            {
+                _movingLeft = false;
+            }
+
+            if (transform.position.x >= _maxX)
+            {
+                _movingLeft = true;
+            }
+        }
     }
 }
