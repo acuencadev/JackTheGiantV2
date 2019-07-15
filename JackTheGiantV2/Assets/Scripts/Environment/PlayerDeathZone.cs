25d58a7bb8a9d34b541286dec416f5d388fbe7d4
Amador Cuenca <sphi02ac@gmail.com>
2019-07-15 07:25:44 -0500

    Create the player death zones

---
+++ JackTheGiantV2/Assets/Scripts/Environment/PlayerDeathZone.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Mantelabs.JackTheGiant.Environment
+{
+    public class PlayerDeathZone : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                //TODO: Stop the game.
+            }
+        }
+    }
+}
